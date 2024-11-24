d422bb8a60200a11ea124db9cb0a4dcd456e7474
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-11-24 19:16:53 +0100

    fix: eval start with updated main

--- src/main.py
+++ src/main.py
@@ -1,32 +1,46 @@
+operand1 = None
+operator = None
+operand2 = None
+
 def main():
+    ask_user_input()
+    result = calculate(operand1, operator, operand2)
+    display_result(operand1, operator, operand2, result)
+
+def ask_user_input():
     # Get first operand from the user
+    global operand1
     operand1 = float(input("Enter the first operand: "))
 
+    global operator
     # Get the operator from the user
     operator = input("Enter an operator (+, -, *, /): ")
 
+    global operand2
     # Get second operand from the user
     operand2 = float(input("Enter the second operand: "))
 
+def calculate(ope1, oper, ope2):
     # Perform the operation based on the operator
-    if operator == '+':
-        result = operand1 + operand2
-    elif operator == '-':
-        result = operand1 - operand2
-    elif operator == '*':
-        result = operand1 * operand2
-    elif operator == '/':
-        if operand2 == 0:
-            print("Error: Division by zero is undefined.")
+    match oper:
+        case '+':
+            res = ope1 + ope2
+        case '-':
+            res = ope2 - ope2
+        case '*':
+            res = ope1 * ope2
+        case '/':
+            if ope2 == 0:
+                print("Error: Division by zero is undefined.")
+                return
+            res = ope1 / ope2
+        case _:
+            print("Invalid operator.")
             return
-        result = operand1 / operand2
-    else:
-        print("Invalid operator.")
-        return
+    return res
 
-    # Print the result
-    print("Result:", result)
-
+def display_result(op1, ope, ope2, res):
+    print(str(op1) + " " + ope + " " + str(ope2) + " = " + str(res))
 
 # Call the main function to run the program
 main()
